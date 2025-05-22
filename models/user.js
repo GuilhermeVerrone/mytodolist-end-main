4028c2fd72e3e1103d9c9cd936087efd87e42aca
Guilherme Verrone <Guilherme.Verrone@outlook.com>
2025-05-22 00:49:32 -0300

    EU NAO AGUENTO MAISSS

--- models/user.js
+++ models/user.js
@@ -1,15 +1,20 @@
 var mongoose = require('mongoose');
+
 var userSchema = new mongoose.Schema({
- nome: {
- unique: true,
- type: String
- },
- senha: {
- type: String
- },
+  nome: {
+    unique: true,
+    type: String
+  },
+  senha: {
+    type: String
+  },
+  admin: {                 
+    type: Boolean,
+    default: false
+  }
 },
- {
- versionKey: false
- }
-);
-module.exports = mongoose.model('User', userSchema)
+{
+  versionKey: false
+});
+
+module.exports = mongoose.model('User', userSchema);
